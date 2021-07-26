55156ad0dc566d9bc4f898657e7b55c81dd74f25
Kumodatsu <dragonkumo@gmail.com>
2021-07-26 18:33:25 +0200

    Animate Jacob

--- dream-within-a-dream/scripts/jacob.gd
+++ dream-within-a-dream/scripts/jacob.gd
@@ -13,7 +13,9 @@ func _ready():
     pass
 
 func _physics_process(_delta: float):
-    if is_on_floor():
+    var on_floor = is_on_floor()
+    # Movement
+    if on_floor:
         jumps = max_jumps
     velocity.x = 0.0
     if Input.is_action_pressed("player_move_left"):
@@ -24,6 +26,8 @@ func _physics_process(_delta: float):
     if Input.is_action_just_pressed("player_jump") and can_jump():
         jump()
     velocity = move_and_slide(velocity, Vector2.UP)
+    # Animation
+    handle_animations(on_floor)
 
 func can_jump() -> bool:
     return jumps > 0
@@ -31,3 +35,17 @@ func can_jump() -> bool:
 func jump():
     jumps -= 1
     velocity.y = -jump_force
+
+func handle_animations(on_floor: bool):
+    var horizontally_still = abs(velocity.x) == 0.0
+    var facing_right       = velocity.x >= 0.0
+    
+    if not horizontally_still:
+        $AnimatedSprite.flip_h = not facing_right
+        
+    if not on_floor:
+        $AnimatedSprite.play("Air")
+    elif not horizontally_still:
+        $AnimatedSprite.play("Walk")
+    else:
+        $AnimatedSprite.play("Idle")
